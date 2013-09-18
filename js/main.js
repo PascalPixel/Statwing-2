21c6cc32531f86c4187c9f338b990da4177e2ece
Pascal Verstegen <pascal@superpencil.com>
2013-09-19 01:19:54 +0200

    Added Instant Visualization animation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
+function cycleImages(){
+      var $active = $('#cycler .active');
+      var $next = ($active.next().length > 0) ? $active.next() : $('#cycler img:first');
+      $next.css('z-index',2);//move the next image up the pile
+      $active.fadeOut(1500,function(){//fade out the top image
+	      $active.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
+          $next.css('z-index',3).addClass('active');//make the next image the top one
+      });
+    }
+
 $(document).ready(function() {
 	var barreset = function() {
 			$('.chart-male .bar-1, .chart-male .bar-2, .chart-male .bar-3, .chart-male .bar-4, .chart-male .bar-5, .chart-male .bar-6, .chart-male .bar-7, .chart-male .bar-8, .chart-male .bar-9, .chart-male .bar-10, .chart-male .bar-11, .chart-male .bar-12, .chart-male .bar-13, .chart-male .bar-14, .chart-male .bar-15, .chart-male .bar-16, .chart-female .bar-1, .chart-female .bar-2, chart-female .bar-3, .chart-female .bar-4, .chart-female .bar-5, .chart-female .bar-6, .chart-female .bar-7, .chart-female .bar-8, .chart-female .bar-9, .chart-female .bar-10, .chart-female .bar-11, .chart-female .bar-12, .chart-female .bar-13, .chart-female .bar-14, .chart-female .bar-15, .chart-female .bar-16').css("height", "5px");
 		};
+	
 	//Animation Gender v. Satisfaction
 	var genderVSsatisfaction = function() {
 			$('.chart-male .bar-1').css("height", "5%");
@@ -37,6 +48,7 @@ $(document).ready(function() {
 			$('.chart-female .bar-15').css("height", "10%");
 			$('.chart-female .bar-16').css("height", "5%");
 		};
+	
 	// Relate selected buttons function
 	var relate = function() {
 			if ($('#compareBtn1').hasClass('active') && $('#compareBtn2').hasClass('active')) {
@@ -55,15 +67,18 @@ $(document).ready(function() {
 				$('.compare6').fadeIn();
 			}
 		};
+	
 	//The array that will always hold the two most recent selected buttons
 	var activeBtns = [];
 	$('.btn-compare').bind('click', function() {
 		var $btn = $(this);
+		
 		//1. Hide the right box, reset arrow
 		$('.compare-result').fadeOut();
 		barreset();
 		$('.line-double').css("background-image", "url(img/line-double.png)");
 		$('.line-merge').css("background-image", "url(img/line-merge.png)");
+		
 		//2. Push in the array if button is pushed
 		if (!$btn.hasClass('active')) {
 			activeBtns.push($btn); //add clicked elem to array
@@ -75,11 +90,13 @@ $(document).ready(function() {
 				return elem.attr('class') !== $btn.attr('class'); //remove clicked element from the array. I compare classes as they provide a clean result, objects don't work
 			});
 		}
+		
 		//3. Run the show/hide buttons mambo jumbo
 		$('.btn-compare').removeClass('active');
 		for (x in activeBtns) {
 			activeBtns[x].addClass('active');
 		}
+		
 		//4. Change background lines
 		if ($('#compareBtn1').hasClass('active') && $('#compareBtn2').hasClass('active')) {
 			$('.line-double').css("background-image", "url(img/line-double_1.png)");
@@ -101,14 +118,17 @@ $(document).ready(function() {
 			$('.line-merge').css("background-image", "url(img/line-merge_6.png)");
 		}
 	});
+	
 	//5. Relate button click, compare two active buttons
 	$('.btn-relate').bind('click', function() {
 		relate();
 	});
+	
 	//6. Intro timed animation
 	setTimeout(function() {
 		genderVSsatisfaction();
 	}, 1000);
+	
 	//7. Light up Sign up button after hero scrolled
 	$(document).scroll(function() {
 		var scroll = $(window).scrollTop();
@@ -118,6 +138,10 @@ $(document).ready(function() {
 			$(".btnSignup").stop().css({"border-color":"#aec4cb","color":"#aec4cb"});
 		}
 	});
+	
 	//8. Activate SKROLLR
 	var s = skrollr.init();
+	
+	//9. graph animation instant viz
+	setInterval('cycleImages()', 4000);
 });
